b29a8a7d9d693679bcdd96fec0e3cbba52b39d24
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-30 14:02:41 +0530

    Bio layout done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Bio from "./Components/Bio";
 function App() {
   return (
     <div className="">
-      <header className="">My portfolio</header>
-      <Bio />
+      <header className=" fw-bold display-1 py-3 mb-4 border-bottom">
+        <div className="container d-flex flex-wrap justify-content-center">
+          <svg className="bi me-2" width="40" height="32"></svg>
+          <span className="fs-4">My Portfolio</span>
+        </div>
+      </header>
+      <div className="container">
+        <Bio />
+      </div>
     </div>
   );
 }
--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function Bio() {
-  return <div className="">CARD</div>;
+  return (
+    <div
+      className="border border-3 border-dark shadow-lg p-3 mb-5 bg-body rounded"
+      style={{
+        backgroundImage: "url('https://source.unsplash.com/iE71-TMrrkE')",
+      }}
+    >
+      <div
+        className="m-4 card border border-dark shadow-lg p-3 mb-5 bg-body rounded"
+        style={{ width: "18rem" }}
+      >
+        <div className="card-body">
+          <h5 className="card-title">Abhinav Choudhary</h5>
+          <h6 className="card-subtitle mb-2 text-muted">
+            Computer Science Student
+          </h6>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          {/* <FontAwesomeIcon icon="fa-brands fa-github" /> */}
+          <a href="https://github.com/Abhinav-Chdhary" className="card-link">
+            github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/abhinavchdhary/"
+            className="card-link"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
