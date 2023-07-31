6811d3a6574ece8374f9229fb7464bee4e21532e
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-31 12:45:10 +0530

    Top Bar gradient upgrades

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function TopBar() {
   return (
     <div>
-      <header className="bg-dark text-light fw-bold display-1 py-3 mb-4 border-bottom">
+      <header className="position-sticky bg-dark text-light fw-bold display-1 py-3 mb-4 border-bottom dagger">
         <div className="container d-flex flex-wrap justify-content-center">
-          <span
+          <div
             className="fs-1"
             style={{ "font-family": "'Permanent Marker', cursive" }}
           >
             My Portfolio
-          </span>
+          </div>
         </div>
       </header>
       <figure className="text-center">
