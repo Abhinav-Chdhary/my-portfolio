fe9e3f88f13f2cc9ba00f2ef894c5d6e27cc383d
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-31 13:36:00 +0530

    Sticky top-bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Bio from "./Components/Bio";
 import TopBar from "./Components/TopBar";
 import Carousel from "./Components/Carousel";
+import Quote from "./Components/Quote";
 
 function App() {
   return (
     <div>
       <TopBar />
+      <Quote />
       <div className="container">
         <Bio />
         <hr />
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function TopBar() {
   return (
-    <div>
-      <header className="position-sticky top-0 bg-dark text-light fw-bold display-1 py-3 mb-4 border-bottom dagger">
-        <div className="container d-flex flex-wrap justify-content-center">
+    <div className="sticky-top">
+      <header className=" bg-dark text-light fw-bold display-1 py-3 mb-4 border-bottom dagger">
+        <div className=" container d-flex flex-wrap justify-content-center">
           <div
             className="fs-1"
             style={{ "font-family": "'Permanent Marker', cursive" }}
@@ -13,14 +13,6 @@ export default function TopBar() {
           </div>
         </div>
       </header>
-      <figure className="text-center">
-        <blockquote className="blockquote">
-          <p>"Condensing fact from the vapor of nuance"</p>
-        </blockquote>
-        <figcaption className="blockquote-footer">
-          Juanita, <cite title="Source Title">Snow Crash</cite>
-        </figcaption>
-      </figure>
     </div>
   );
 }
