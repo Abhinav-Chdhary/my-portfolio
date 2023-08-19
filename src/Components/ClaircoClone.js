76a566550a4916fff3c7309e4291081bf7236e2a
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-19 19:47:09 +0530

    Update ClaircoClone.js

--- src/Components/ClaircoClone.js
+++ src/Components/ClaircoClone.js
@@ -1,5 +1,67 @@
 import React from "react";
 
 export default function ClaircoClone() {
-  return <div>ClaircoClone</div>;
+  return (
+    <div
+      id="myCarousel"
+      className="carousel slide container pt-2 pb-2"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators bg-dark">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src="https://github.com/Abhinav-Chdhary/my-portfolio/blob/main/public/clairco1.png?raw=true"
+            className="d-block w-100"
+            alt="..."
+          />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
 }
