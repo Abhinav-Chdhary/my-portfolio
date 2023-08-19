fbb4467c3683ef1fd116df9958af5b6eef409017
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-19 19:36:10 +0530

    Added files to display 2 new apps

---
+++ src/Components/ClaircoClone.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ClaircoClone() {
+  return <div>ClaircoClone</div>;
+}
--- src/Screens/MoreScreen.js
+++ src/Screens/MoreScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
+import ClaircoClone from "../Components/ClaircoClone";
+import Seneca from "../Components/Seneca";
 
 export default function MoreScreen() {
-  return <div>MoreScreen</div>;
+  return (
+    <div>
+      <ClaircoClone />
+      <Seneca />
+    </div>
+  );
 }
