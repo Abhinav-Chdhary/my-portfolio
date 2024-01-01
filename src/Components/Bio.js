489be12f6c9949e947e92db1c0f756539b294052
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-01 09:22:04 +0530

    Update Bio.js

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -10,15 +10,17 @@ export default function Bio() {
     >
       <div
         className="m-4 card border border-dark shadow-lg p-3 mb-5 bg-body rounded"
-        style={{ width: "18rem" }}
+        style={{ width: "20rem" }}
       >
         <div className="card-body">
           <h5 className="card-title fw-bold">Abhinav Choudhary</h5>
           <h6 className="card-subtitle mb-2 text-muted">CS Student</h6>
           <p className="card-text">
-            Hello & welcome to my portfolio website! Here, I proudly present my
-            collection of personal projects that I'm excited to share with you.
-            I sincerely hope you find them inspiring.
+            Hello & welcome to my portfolio website! I’m Abhinav, a passionate
+            computer science student with a dream of becoming a tech wizard. My
+            fascination with technology began at a young age, and it has been a
+            thrilling journey ever since. I believe that through innovation and
+            creativity, we can shape a better and more connected world.
           </p>
         </div>
       </div>
