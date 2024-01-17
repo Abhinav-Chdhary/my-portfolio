2f9d024d39f1846e554389eb94e375a647abbeee
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-17 13:51:12 +0530

    About me section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import TopBar from "./components/TopBar";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import MoonScene from "./components/MoonScene";
+import AboutMe from "./components/AboutMe";
 
 export default function App() {
   return (
     <div>
       <TopBar />
       <MoonScene />
+      <AboutMe />
     </div>
   );
 }
---
+++ my-portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import avatarImage from "../assets/SitePhoto.jpg";
+
+export default function AboutMe() {
+  return (
+    <div className="bg-blue-950">
+      <div className="p-8 flex flex-col md:flex-row justify-around container mx-auto">
+        <div className="flex-1 justify-center">
+          <h2 className="text-3xl font-bold mb-4 text-white">About Me</h2>
+          <p className="text-gray-200 mx-auto">
+            Hi there! I'm Abhinav Choudhary, a passionate developer who loves
+            creating awesome web applications. I have experience with front-end
+            technologies like React and enjoy exploring new technologies to
+            enhance my skills.
+          </p>
+          <p className="text-gray-200 mt-2">
+            In my free time, I enjoy solving coding challenges, making games,
+            and staying up-to-date with the latest trends in the tech world.
+          </p>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <img
+            src={avatarImage}
+            alt="Avatar"
+            className="rounded-full object-cover w-full md:w-64 h-auto mb-4 md:mb-0"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
