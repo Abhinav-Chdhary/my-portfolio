c381e13ffc0279e9039b9ee860ebc8d63e53da13
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-18 16:56:19 +0530

    Update Experience.jsx

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -5,13 +5,19 @@ import ClaircoImg from "../assets/Clairco.jpg";
 const Experience = () => {
   return (
     <div className="bg-blue-950 p-8 text-white ">
-      <div className="container mx-auto 2xl:max-w-5xl">
+      <div className="container mx-auto max-w-4xl">
         <h1 className="text-3xl font-bold mb-4">Experience</h1>
 
-        <div className="mb-8 flex">
+        <div className="mb-8 flex flex-col-reverse md:flex-row">
+          <img
+            src={GEUimg}
+            alt="GEU image"
+            className="rounded-full object-cover w-full md:w-48 h-auto my-2 md:mb-0 mx-auto md:mx-2"
+            style={{ boxShadow: "0 4px 8px rgba(255, 255, 255, 0.7)" }}
+          />
           <div>
             <h2 className="text-2xl font-bold">Graphic Era University</h2>
-            <h3 className="text-lg font-semibold text-gray-400">
+            <h3 className="font-semibold text-gray-400 text-sm md:text-lg">
               CS Student | 2021-2025 | SCGPA: 8.47
             </h3>
             <p className="text-white">
@@ -22,32 +28,26 @@ const Experience = () => {
               and active participation in computer science discussions.
             </p>
           </div>
-          <img
-            src={GEUimg}
-            alt="GEU image"
-            className="rounded-full object-cover w-full md:w-48 h-auto my-2 md:mb-0 mx-2"
-            style={{ boxShadow: "0 4px 8px rgba(255, 255, 255, 0.7)" }}
-          />
         </div>
 
-        <div className="text-right flex">
+        <div className="mb-8 flex flex-col-reverse md:flex-row">
           <img
             src={ClaircoImg}
             alt="Clairco Image"
-            className="rounded-full object-cover w-full md:w-48 h-auto my-2 md:mb-0 bg-white mx-2"
+            className="rounded-full object-cover w-full md:w-48 h-auto my-2 md:mb-0 mx-auto md:mx-2"
             style={{ boxShadow: "0 4px 8px rgba(255, 255, 255, 0.7)" }}
           />
           <div>
             <h2 className="text-2xl font-bold">Clairco</h2>
-            <h3 className="text-lg font-semibold text-gray-400">
-              Software Development Intern | Jun 23 - Aug 23
+            <h3 className="text-sm md:text-lg font-semibold text-gray-400">
+              Software Development Intern | Jun'23 - Aug'23
             </h3>
             <p className="text-white">
-              Clairco (Aliferous Technologies Pvt Ltd) is a Bangalore startup.
-              As a Software Developer intern, I contributed to interactive
-              applications using React, MongoDB, Express.js, and Node.js,
-              gaining hands-on experience across the software development
-              lifecycle.
+              Clairco (Aliferous Technologies Pvt Ltd) is a Bangalore based
+              startup. As a Software Developer intern, I contributed to
+              interactive applications using React, MongoDB, Express.js, and
+              Node.js, gaining hands-on experience across the software
+              development lifecycle.
             </p>
           </div>
         </div>
